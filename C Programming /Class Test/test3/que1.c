254c00fdaed00a1bfd77d654d4bc393862c30a50
pranjal-punde <pundepranjal29@gmail.com>
2025-10-08 22:57:50 +0530

    Add files via upload

---
+++ C Programming /Class Test/test3/que1.c
@@ -0,0 +1,17 @@
+void main()
+{
+  for(int i=10;i<=25;i++)
+  {
+  	if(i%2==0)
+  	{
+  		printf("%d ",i);
+  		
+	  }
+}
+printf("\n");
+    for(int j=1;j<=25;j++)
+	  {
+	  	if(j%2!=0)
+	  	printf("%d ",j);
+	  }
+  }
---
+++ C Programming /Class Test/test3/que2.c
@@ -0,0 +1,9 @@
+void main()
+{
+	int sum=0;
+	for(int n=1;n<=5;n+=2)
+	{
+	  sum=sum+n;
+	}
+	printf("%d",sum);
+}
